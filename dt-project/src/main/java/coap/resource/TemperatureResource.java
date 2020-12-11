e66278fd7edc5f57ab3a112bc06a305216915553
Federica-B <federica.barbi99@gmail.com>
2020-12-11 16:37:51 +0100

    fixing .gitignore

---
+++ dt-project/src/main/java/coap/resource/TemperatureResource.java
@@ -0,0 +1,158 @@
+package coap.resource;
+
+import coap.utils.CoreInterfaces;
+import coap.utils.SenMLPack;
+import coap.utils.SenMLRecord;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.eclipse.californium.core.CoapResource;
+import org.eclipse.californium.core.coap.CoAP;
+import org.eclipse.californium.core.coap.MediaTypeRegistry;
+import org.eclipse.californium.core.server.resources.CoapExchange;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.*;
+
+
+public class TemperatureResource extends CoapResource {
+
+    private final static Logger logger = LoggerFactory.getLogger(TemperatureResource.class);
+
+    private static final Number SENSOR_VERSION = 0.1;
+
+    private static final String OBJECT_TITLE = "TemperatureSensor";
+
+    private static final String RESOURCE_TYPE = "dt.sensor.temperature";
+
+    private static final long SENSOR_UPDATE_TIME_MS = 1000; //1Hz for update
+
+    private static final int RESOURCE_MAX_AGE_SECONDS = 1;
+
+    //Cel	degrees Celsius     float	RFC-AAAA -- Number in JASON / double in XML
+    private Number temperature;
+
+    private String deviceId;
+
+    private String TEMPERATURE_UNIT = "Cel";
+
+    private ObjectMapper objectMapper;
+
+    private List<Double> temperatureList = new ArrayList<Double>();
+
+    private ListIterator<Double> temperatureListIterator;
+
+    //TODO add the others file in the iteration
+    private static final String TEMPERATURE_FILE_NAME = "data/TS1.txt";
+
+
+    public TemperatureResource(String deviceId, String name) {
+        super(name);
+
+        init();
+
+        this.deviceId = deviceId;
+
+        //Ignore Null Field
+        this.objectMapper = new ObjectMapper();
+        this.objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        //set Observable
+        setObservable(true);
+        //notification type CON
+        setObserveType(CoAP.Type.CON);
+
+        getAttributes().setTitle(OBJECT_TITLE);
+        getAttributes().setObservable(); //set obs in the link-format but can also not be obs with this value
+
+        //resource attributes
+        //TODO better write RESOURCE_TYPE
+        getAttributes().addAttribute("rt", RESOURCE_TYPE);
+        getAttributes().addAttribute("if", CoreInterfaces.CORE_S.getValue());
+
+        //periodic update
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if(temperatureListIterator.hasNext()) {
+                    temperature = temperatureListIterator.next();
+                    //notification that the value has changed
+                   changed();
+                }else{
+                    logger.info("End of the document");
+                }
+            }
+        }, 0, SENSOR_UPDATE_TIME_MS);
+
+
+    }
+
+
+
+    private void init() {
+        try{
+
+            BufferedReader cvsReader = new BufferedReader(new FileReader(TEMPERATURE_FILE_NAME));
+            String row;
+            while ((row = cvsReader.readLine()) != null){
+                String [] line = row.split("\t");
+                for (String s : line) {
+                    temperatureList.add(Double.parseDouble(s));
+                }
+            }
+            logger.info("Temperature File correctly loaded! Size: {}", this.temperatureList.size());
+
+            this.temperatureListIterator = this.temperatureList.listIterator();
+
+        }catch(Exception e){
+            logger.error("Error init Resource Object! Msg: {}", e.getLocalizedMessage());
+        }
+    }
+
+    private Optional<String> getJsonSenmlResponse(){
+        try{
+            SenMLPack senMLPack = new SenMLPack();
+            SenMLRecord senMLRecord = new SenMLRecord();
+
+            senMLRecord.setBaseName(String.format("%s:%s", this.deviceId, this.getName()));
+            senMLRecord.setVersion(SENSOR_VERSION);
+            senMLRecord.setUnit(TEMPERATURE_UNIT);
+            senMLRecord.setValue(temperature);
+            senMLRecord.setTime(System.currentTimeMillis());
+
+            //Pack
+            senMLPack.add(senMLRecord);
+
+            return Optional.of(this.objectMapper.writeValueAsString(senMLPack));
+        }catch (Exception e){
+            logger.error("Error Generiting SenML Record : {}", e.getLocalizedMessage());
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public void handleGET(CoapExchange exchange) {
+        exchange.setMaxAge(RESOURCE_MAX_AGE_SECONDS);
+
+        //two type of response: JSON + SenML and plain
+        //code senml-json  110 - json 50 - plain 0
+        if(exchange.getRequestOptions().getAccept() == MediaTypeRegistry.APPLICATION_SENML_JSON ||
+                exchange.getRequestOptions().getAccept() == MediaTypeRegistry.APPLICATION_JSON){
+            Optional<String> senmlPayload = getJsonSenmlResponse();
+
+            if(senmlPayload.isPresent())
+                exchange.respond(CoAP.ResponseCode.CONTENT, senmlPayload.get(), exchange.getRequestOptions().getAccept());
+            else
+                exchange.respond(CoAP.ResponseCode.INTERNAL_SERVER_ERROR);
+        }else{
+            if(temperature != null)
+                exchange.respond(CoAP.ResponseCode.CONTENT, String.valueOf(temperature),MediaTypeRegistry.TEXT_PLAIN);
+            else
+                exchange.respond(CoAP.ResponseCode.INTERNAL_SERVER_ERROR);
+
+        }
+    }
+
+}
