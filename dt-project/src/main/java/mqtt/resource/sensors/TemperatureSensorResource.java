c960e4bc63ec61c423d4bff71e6342084e020095
Federica-B <federica.barbi99@gmail.com>
2020-12-11 12:11:20 +0100

    prova

---
+++ dt-project/src/main/java/mqtt/resource/sensors/TemperatureSensorResource.java
@@ -0,0 +1,111 @@
+package mqtt.resource.sensors;
+
+import mqtt.model.TemperatureDescriptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import mqtt.resource.DTObjectResource;
+import mqtt.resource.ResourceDataListener;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.*;
+
+public class TemperatureSensorResource extends DTObjectResource<TemperatureDescriptor> {
+
+    private static final Logger logger = LoggerFactory.getLogger(TemperatureSensorResource.class);
+
+    //5 seconds of update
+    private static final long UPDATE_PERIOD = 5000;
+
+    //optional --> seconds before starting the periodic update task.
+    private static final long TASK_DELAY_TIME = 5000;
+
+    public static final String RESOURCE_TYPE = "iot::sensors:temperature";
+
+    //C:\Users\User\Desktop\Uni\Terzo anno\IoT\dt-project\data\TS1.txt
+    public static final String TEMPERATURE_FILE_NAME = "data/TS1.txt";
+
+    private List<Double> dataList = new ArrayList<Double>();
+
+    private ListIterator<Double> dataListIterator;
+
+    private Timer updateTimer = null;
+
+    private TemperatureDescriptor updateTemperatureDescriptor = null;
+
+    public TemperatureSensorResource() {
+        super(UUID.randomUUID().toString(), TemperatureSensorResource.RESOURCE_TYPE);
+        init();
+    }
+
+    public TemperatureSensorResource(String id, String type) {
+        super(id, type);
+        init();
+    }
+
+    private void init() {
+        try{
+            this.updateTemperatureDescriptor = new TemperatureDescriptor();
+
+            BufferedReader cvsReader = new BufferedReader(new FileReader(TEMPERATURE_FILE_NAME));
+            String row;
+            while ((row = cvsReader.readLine()) != null){
+                String [] line = row.split("\t");
+                for (String s : line) {
+                    dataList.add(Double.parseDouble(s));
+                }
+            }
+            logger.info("Temperature File correctly loaded! Size: {}", this.dataList.size());
+
+            this.dataListIterator = this.dataList.listIterator();
+
+            startPeriodicEventValueUpdateTask();
+
+        }catch(Exception e){
+            logger.error("Error init Resource Object! Msg: {}", e.getLocalizedMessage());
+        }
+    }
+
+    private void startPeriodicEventValueUpdateTask() {
+        try{
+            logger.info("Starting periodic Update Task with Period {} ms", UPDATE_PERIOD);
+
+            this.updateTimer = new Timer();
+            this.updateTimer.schedule(new TimerTask() {
+
+                @Override
+                public void run() {
+                    if(dataListIterator.hasNext()) {
+                        Double currentTemperature = dataListIterator.next();
+                        updateTemperatureDescriptor = new TemperatureDescriptor(
+                                currentTemperature, TemperatureDescriptor.FILE_TEMPERATURE_PROVIDER);
+                    notifyUpdate(updateTemperatureDescriptor);
+                    }else{
+                        logger.info("End of the document");
+                    }
+                }
+                }, TASK_DELAY_TIME, UPDATE_PERIOD);
+        }catch (Exception e){
+            logger.error("Error executing periodic resource value {}", e.getLocalizedMessage());
+        }
+    }
+
+    @Override
+    public TemperatureDescriptor loadUpdateValue() {
+        return this.updateTemperatureDescriptor;
+    }
+
+    public static void main(String[] args) {
+        TemperatureSensorResource temperatureSensorResource = new TemperatureSensorResource();
+        temperatureSensorResource.addDataListener(new ResourceDataListener<TemperatureDescriptor>() {
+            @Override
+            public void onDataChanged(DTObjectResource<TemperatureDescriptor> resource, TemperatureDescriptor updatedValue) {
+                if(resource != null && updatedValue != null)
+                    logger.info("Device: {} --> New Value Recived: {}", resource.getId(), updatedValue);
+                else
+                    logger.error("onDataChangedCallback --> Null Resource or Update Value");
+
+            }
+        });
+    }
+}
